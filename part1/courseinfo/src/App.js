e2b34a383e9b0d618a5b1b009a52e1d5e45cc947
Denis Filonov <denis@filonov.pro>
2023-04-05 15:32:52 +0200

    ex 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -19,42 +19,40 @@ const Part = (props) => (
   </p>
 )
 
-const Content = (props) => {
-  let out = []
+const Content = (props) => (
+  <>
+    {props.parts.map(element =>
+      (<Part key={element.name} name={element.name} exercises={element.exercises} />)
+    )}
+  </>
+)
 
-  console.log(out)
-  return (
-    <>
-      {props.parts.map(element =>
-        (<Part key={element.name} name={element.name} exercises={element.exercises} />)
-      )}
-    </>
-  )
-}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
