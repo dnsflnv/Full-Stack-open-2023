92cc139c9b87aa645d415b43568f262abee8350e
Denis Filonov <denis@filonov.pro>
2023-04-05 10:32:20 +0200

    ex 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,6 +4,10 @@ const Header = (props) => {
   )
 }
 
+const Total = (props) => (
+  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+)
+
 const Content = (props) => {
   return (
     <>
@@ -33,7 +37,7 @@ const App = () => {
     <div>
       <Header caption={course} />
       <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
