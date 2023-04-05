524cabb31115aed0d5ea7ebbb1ae448ba80e0244
Denis Filonov <denis@filonov.pro>
2023-04-05 10:35:18 +0200

    ex 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,18 +8,19 @@ const Total = (props) => (
   <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
 )
 
+const Part = (props) => (
+  <p>
+    {props.part} {props.ex}
+  </p>
+)
+
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} ex={props.exercises1} />
+      <Part part={props.part2} ex={props.exercises2} />
+      <Part part={props.part2} ex={props.exercises3} />
+
     </>
   )
 }
