69c255eebcd95b62a1e8eccaf97bce585911bf11
Denis Filonov <denis@filonov.pro>
2023-07-11 18:53:24 +0200

    Ex 3.5-3.6

--- part3/server/index.js
+++ part3/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     "id": 1,
@@ -48,9 +50,48 @@ app.get('/api/persons/:id', (req, res) => {
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+  response.status(204).end();
+});
+
+function generateId() {
+  const min = Math.ceil(1);
+  const max = Math.floor(999);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    });
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(409).json({
+      error: 'name already exists'
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person);
+
+  response.json(person);
 })
 
 const PORT = 3001;
