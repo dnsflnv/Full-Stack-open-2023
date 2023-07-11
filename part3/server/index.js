ce7bf250718b3b6120488bd09a86878d57aeb427
Denis Filonov <denis@filonov.pro>
2023-07-11 18:02:33 +0200

    Ex 3.3

--- part3/server/index.js
+++ part3/server/index.js
@@ -35,7 +35,19 @@ app.get('/info', (request, response) => {
     `<p>${currentDate}</p>`);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    res.json(person);
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
