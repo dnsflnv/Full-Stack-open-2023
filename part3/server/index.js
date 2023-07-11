9a66f6e4d623e9187786c969fa97347af4350730
Denis Filonov <denis@filonov.pro>
2023-07-11 19:39:34 +0200

    Ex 3.8

--- part3/server/index.js
+++ part3/server/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body), // Added line to show request body
+  ].join(' ')
+}));
+
+
+
 
 let persons = [
   {
